dc19b5e0163be0373dc95206df89a858916a6b83
cpr5855 <esor.rehpotsirhc@gmail.com>
2023-06-07 16:32:24 -0400

    Update Portal_Knowledgebase_Title_Updater.user.js

--- Portal_Knowledgebase_Title_Updater.user.js
+++ Portal_Knowledgebase_Title_Updater.user.js
@@ -3,6 +3,7 @@
 // @description    Update Title and Article Title to be more helpful.
 // @match        https://portal.nutanix.com/page/documents/kbs*
 // @version      1.2
+// @updateURL https://github.com/cpr5855/userscripts/raw/main/Portal_Knowledgebase_Title_Updater.user.js
 // @downloadURL https://github.com/cpr5855/userscripts/raw/main/Portal_Knowledgebase_Title_Updater.user.js
 // ==/UserScript==
 
